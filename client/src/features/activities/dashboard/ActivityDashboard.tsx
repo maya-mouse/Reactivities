355821541dcc943869218c083e500fd82a7508ef
maya-mouse <120753794+maya-mouse@users.noreply.github.com>
2025-09-12 13:11:08 +0300

    CRUD app in React + React Query + Axios

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,39 @@
+import { Grid2} from '@mui/material'
+import ActivityList from './ActivityList'
+import ActivityDetail from '../details/ActivityDetail'
+import ActivityForm from '../form/ActivityForm';
+
+
+type Props = {
+    activities: Activity[],
+    selectActivity: (id: string) => void;
+    cancelSelectAtivity: () => void;
+    selectedActivity: Activity | undefined;
+    openForm: (id: string)=> void;
+    closeForm: ()=> void;
+    editMode:boolean;
+}
+
+export default function ActivityDashboard(
+  {activities, selectActivity, cancelSelectAtivity, 
+    selectedActivity,openForm, closeForm, editMode}: Props) {
+  return (
+    <Grid2 container spacing={3}>
+<Grid2 size={7}>
+     <ActivityList activities={activities}
+     selectActivity={selectActivity}
+     />
+</Grid2>
+<Grid2 size={5}>
+  {selectedActivity && !editMode && <ActivityDetail 
+  selectedActivity={selectedActivity}
+  cancelSelectActivity={cancelSelectAtivity} 
+  openForm={openForm}/>}
+
+  {editMode && 
+  <ActivityForm closeForm={closeForm} activity={selectedActivity} 
+  />}
+</Grid2>
+    </Grid2>
+  )
+}
---
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,58 @@
+
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import type { FormEvent } from "react";
+import { useActivities } from "../../../lib/hooks/useActivities";
+
+type Props = {
+   closeForm : ()=> void;
+  activity?: Activity;
+}
+
+export default function ActivityForm({closeForm, activity }: Props) {
+  const {updateActivity, createActivity} = useActivities();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    const formData = new FormData(event.currentTarget);
+
+    const data: {[key: string]: FormDataEntryValue} = {}
+    formData.forEach((value,key) =>{
+      data[key] = value;
+    })
+
+    if(activity) {
+      data.id = activity.id;
+      await updateActivity.mutateAsync(data as unknown as Activity);
+      closeForm();
+    }else{
+      await createActivity.mutateAsync(data as unknown as Activity);
+      closeForm();
+    }
+
+  }
+
+  return (
+   <Paper sx={{borderRadius: 3, padding: 3}}>
+    <Typography variant="h5" gutterBottom color="primary">Create activity</Typography>
+    <Box component='form'
+    onSubmit={handleSubmit} 
+    sx={{display: 'flex', flexDirection: 'column', gap: 3}}>
+        <TextField name='title' label='Title' defaultValue={activity?.title}/>
+        <TextField name='description' label='Description' defaultValue={activity?.description} multiline rows={3}/>
+        <TextField name='category' label='Category' defaultValue={activity?.category} />
+        <TextField name='date' label='Date' defaultValue={activity?.date
+          ? new Date(activity.date).toISOString().split('T')[0]
+          : new Date().toISOString().split('T')[0]
+        } type='date'/>
+        <TextField name='city' label='City' defaultValue={activity?.city} />
+        <TextField name='venue' label='Venue' defaultValue={activity?.venue} />
+        <Box display='flex' justifyContent='end'>
+         <Button color='inherit'onClick={closeForm}>Cancel</Button>
+         <Button type='submit' color='success' variant='contained'
+         disabled={updateActivity.isPending|| createActivity.isPending}>Submit</Button>
+        </Box>
+    </Box>
+   </Paper>
+  )
+}
