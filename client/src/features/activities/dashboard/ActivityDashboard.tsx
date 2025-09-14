10403b5962dba5c25406e0904332a4aa1721904c
maya-mouse <120753794+maya-mouse@users.noreply.github.com>
2025-09-14 23:05:50 +0300

    Styling UI

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,7 +1,6 @@
 import { Grid2} from '@mui/material'
 import ActivityList from './ActivityList'
-import ActivityDetail from '../details/ActivityDetail'
-import ActivityForm from '../form/ActivityForm';
+import ActivityFilters from './ActivityFilters';
 
 
 
@@ -10,11 +9,11 @@ export default function ActivityDashboard() {
 
   return (
     <Grid2 container spacing={3}>
-<Grid2 size={7}>
+<Grid2 size={8}>
      <ActivityList/>
 </Grid2>
-<Grid2 size={5}>
- Activity filters go here
+<Grid2 size={4}>
+ <ActivityFilters />
 </Grid2>
     </Grid2>
   )
